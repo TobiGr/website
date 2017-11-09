70828e4ae9928077bdd558e4676199e91c95a436
TobiGr <TobiGr@users.noreply.github.com>
2017-11-09 22:27:52 +0100

    App fade animation

--- js/features.js
+++ js/features.js
@@ -1,27 +1,47 @@
-var featureHeight;
+var featureHeight, currentFeature;
 function toggleFeatures(tile = -1){
     if(tile != -1) {
         hideFeatureTiles();
-        showFeatureDetail(tile);
+        //showFeatureDetail(tile);
+        setTimeout(function(){showFeatureDetail(tile);},500);
     }
     else {
-        hideModal();
-        showFeatureTiles();
+        hideFeatureDetails();
+        //showFeatureTiles();
+        setTimeout(function(){showFeatureTiles();},500);
     }
 }
 
 function hideFeatureTiles() {
-    $('#features-tiles').hide();
+    fadeOut('#features-tiles');
+    featureHeight = $('#features1').outerHeight();
+    //$('#features1').css('height', featureHeight + 'px');
+    setTimeout(function(){$('#features1').css('height', featureHeight + 'px');},500);
+    //$('#features-tiles').hide();
 }
 
 function showFeatureDetail(name) {
-    name = '#' + name;
-    $(name).show();
+    //alert(name);
+    currentFeature = '#' + name;
+    $(currentFeature).show();
     $('#feature-controls').show();
-}
-function hideModal() {
+    $(currentFeature).removeClass('fadeOut');
+    $('#feature-controls').removeClass('fadeOut');
     
+}
+function hideFeatureDetails() {
+    //$(currentFeature).hide();
+    fadeOut(currentFeature);
+    fadeOut('#feature-controls');
+    //$('#' + currentFeature).hide();
+    //$('#feature-controls').hide();
 }
 function showFeatureTiles() {
-    
+    $('#features-tiles').removeClass('fadeOut');
+    $('#features-tiles').show();
+}
+
+function fadeOut(el) {
+    $(el).addClass('fadeOut');
+    setTimeout(function(){$(el).hide();},500);
 }
