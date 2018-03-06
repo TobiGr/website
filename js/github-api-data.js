6beec10fcb9593dac71bf201765023a1b3bff96a
TobiGr <TobiGr@users.noreply.github.com>
2018-03-06 13:33:32 +0100

    Design improvements Add prototype for web-api handling

---
+++ js/github-api-data.js
@@ -0,0 +1,16 @@
+var api = {
+    "stable": {
+        "version": "1.0.0",
+        "changelog": "<h1>New</h1>\n<ol><li>Ich habe was</li></ol>"
+    },
+    "beta": {
+        "version": "1.0.0beta",
+        "changelog": "New\nIch habe was"
+    },
+    "githubStats": {
+        "stars": 3030,
+        "contributors": 180,
+        "commits": 1800,
+        "languages": 43
+    }
+};
