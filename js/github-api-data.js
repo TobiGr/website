549e0553e22367d38f240a757e67d830091c297c
TobiGr <TobiGr@users.noreply.github.com>
2018-03-11 18:19:22 +0100

    remove unused code add github api json file

--- js/github-api-data.js
+++ js/github-api-data.js
@@ -1,16 +1,17 @@
 var api = {
-    "stable": {
-        "version": "1.0.0",
-        "changelog": "<h1>New</h1>\n<ol><li>Ich habe was</li></ol>"
-    },
-    "beta": {
-        "version": "1.0.0beta",
-        "changelog": "New\nIch habe was"
-    },
-    "githubStats": {
-        "stars": 3030,
-        "contributors": 180,
-        "commits": 1800,
-        "languages": 43
-    }
-};
+        "stable": {
+            "version": "1.0.0",
+            "changelog": "<h3>New</h3>\n<ul><li>Ich habe was0</li><li>Ich habe was0</li><li>Ich habe was0</li><li>Ich habe was0</li><li>Ich habe was0</li></ul><h3>Fixed</h3>\n<ul><li>Ich habe was0</li><li>Ich habe was0</li><li>Ich habe was0</li><li>Ich habe was0</li><li>Ich habe was1</li></ul><h3>Improved</h3>\n<ul><li>Ich habe was2</li><li>Ich habe was0</li><li>Ich habe was0</li><li>Ich habe was0</li></ul>"
+        },
+        "beta": {
+            "version": "1.0.0beta",
+            "changelog": "New\nIch habe was"
+        },
+        "githubStats": {
+            "stars": 3030,
+            "contributors": 180,
+            "commits": 1800,
+            "languages": 43
+        }
+    };
+
